74ab4ba11beebbd1a94128e1f4d2b978d38e14ae
devopswithal <devopswithalprojects@gmail.com>
2023-03-20 01:04:29 -0500

    Update key config

--- terraform/cap_keys.tf
+++ terraform/cap_keys.tf
@@ -3,14 +3,14 @@ resource "tls_private_key" "ssh" {
   rsa_bits        = "4096"
 }
 
-resource "local_file" "ep_cap_key" {
-  filename        = "ep_cap_key.pem"
+resource "local_file" "cap_ssh_key" {
+  filename        = "cap_ssh_key.pem"
   file_permission = "0400"
   content = tls_private_key.ssh.private_key_pem
 }
 
-resource "aws_key_pair" "ep_cap_ssh" {
-  key_name        = "ep_cap_ssh"
+resource "aws_key_pair" "cap_ssh" {
+  key_name        = "cap_ssh"
   public_key      = tls_private_key.ssh.public_key_openssh
 }
 
