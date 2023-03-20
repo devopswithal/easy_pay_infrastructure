61cec5830b87cb250bcf601bc0a1d6b5befbf341
devopswithal <devopswithalprojects@gmail.com>
2023-03-20 01:07:46 -0500

    Update instances.tf key config

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "bastion" {
   subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = "true"
   security_groups             = [aws_security_group.ssh_access.id]
-  key_name                    = aws_key_pair.ep_cap_ssh.key_name
+  key_name                    = aws_key_pair.cap_ssh.key_name
   user_data                   = <<-EOF
                                 #!/bin/bash
                                 echo "PubkeyAcceptedKeyTypes=+ssh-rsa" >> /etc/ssh/sshd_config.d/10-insecure-rsa-keysig.conf
@@ -31,7 +31,7 @@ resource "aws_instance" "master_nodes" {
   ami                        = var.ami_id
   instance_type              = "t3.small"
   subnet_id                  = element(module.vpc.private_subnets, count.index)
-  key_name                   = aws_key_pair.ep_cap_ssh.key_name
+  key_name                   = aws_key_pair.cap_ssh.key_name
   security_groups            = [aws_security_group.cluster_nodes.id, aws_security_group.master_nodes.id]
 
   tags = {
@@ -45,7 +45,7 @@ resource "aws_instance" "worker_nodes" {
   ami                        = var.ami_id
   instance_type              = "t3.micro"
   subnet_id                  = element(module.vpc.private_subnets, count.index)
-  key_name                   = aws_key_pair.ep_cap_ssh.key_name
+  key_name                   = aws_key_pair.cap_ssh.key_name
   security_groups            = [aws_security_group.cluster_nodes.id, aws_security_group.worker_nodes.id]
 
   tags = {
